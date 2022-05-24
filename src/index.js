8d11dda38a1cb7b0f2afc133fd11053454928b04
Precious Akams <preciousakams@yahoo.com>
2022-05-24 19:18:55 +0100

    homePage created dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+import './index.html';
+
+const row = document.querySelector('.row');
+
+const renderMovie = (allMovies) => {
+  let render = '';
+  allMovies.forEach((movie) => {
+    render += `<div class="col-md-4">
+    <div class="movieImg">
+    <img src="${movie.show.image.medium}" alt="">
+    </div>
+    <p class="title">${movie.show.name} <i class="fa-regular fa-heart"></i></p>
+    <div><span class="likes" id=${movie.show.id}>likes</span></div>
+    <button class="comments">Comments</button>
+    <button class="reserve">Reservations</button>
+   </div>`;
+    row.innerHTML = render;
+  });
+};
+
+const getMovies = async () => {
+  const url = 'https://api.tvmaze.com/search/shows?q=n';
+  const response = await fetch(url);
+  const data = await response.json();
+  renderMovie(data);
+};
+
+getMovies();
